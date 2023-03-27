b6186fd4e84ac0d165c259ff85aabba8430c48b5
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-03-27 10:40:30 -0500

    Health UI Implemented

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    private static UIManager instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("UI Manager is null.");
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField]
+    GameObject[] hearts;
+
+    public void UpdateHealthUI(int health)
+    {
+        if (health >= 1)
+        {
+            hearts[health].SetActive(false);
+        }
+    }
+
+}
